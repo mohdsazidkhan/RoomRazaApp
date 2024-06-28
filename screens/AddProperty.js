ea5b787d499585cfbc4bd09ba52c787f4324f336
SazidKhan <khanmohdsazid@gmail.com>
2024-06-28 10:26:11 +0530

    First Commit

---
+++ screens/AddProperty.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const AddProperty = () => {
+  return (
+    <View>
+      <Text>AddProperty</Text>
+    </View>
+  )
+}
+
+export default AddProperty
